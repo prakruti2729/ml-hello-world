948374d1cf5a3c37b60775751cd8c179b3f4f28c
prakruti2729 <prakruti2729@gmail.com>
2025-06-25 08:39:22 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,7 +3,7 @@ import os
 import psutil
 
 print("Hello, ML World from Jenkins!")
-print(f"Python Version: {platform.python_version()})
+print(f"Python Version: {platform.python_version()}")
 print(f"Operating System: {platform.system()} {platform.release()}")
 print(f"CPU Cores: {os.cpu_count()}")
 print(f"Total RAM: {round(psutil.virtual_memory().total/(1024**3),2)}GB")
